efa30fbaef35271099528c4b01db6cabb77d1290
PRABHAT YADAV <126355394+prabhat60343@users.noreply.github.com>
2025-08-08 00:40:08 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin']
 }))
+app.options('*', cors())
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
